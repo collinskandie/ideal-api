aa9da79967d1d10ac2f2a6400a5de6cda964e403
Martin <mkmartinoes@gmail.com>
2022-05-24 20:23:57 +0300

    ft: get post rotes

--- imdex.js
+++ imdex.js
@@ -8,6 +8,13 @@ app.get('/task', (req, res) =>{
   res.send("Hello World")
 })
 
+app.get('/addnew', (req, res) => {
+  res.send("You can add")
+})
+
+app.get('/delete', (req, res) => {
+  res.send("You can delete")
+})
 
 app.listen(4000, () =>{
   console.log("runing on port 4000")
